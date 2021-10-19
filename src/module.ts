838c5cea428c7e6e0e5a2c797b4e5700462c7d25
SBub <svetaibuben@gmail.com>
2021-10-19 16:04:51 +0200

    fix(cancel-cmd): sending cancel cmd without queueing it
    CANCEL cmd was queue and therefore never send once a user ACCEPTed the request, becuase current opeation contained INSERT_CARD
    info;
    creating a separate flow for the CANCEL cmd

--- src/module.ts
+++ src/module.ts
@@ -179,10 +179,20 @@ export class Aa2Module {
         }
         this.nativeAa2Module.sendCMD(JSON.stringify(command))
       } else {
+        if(command.cmd === 'CANCEL') {
+          this.abortTheFlow(command)
+          return
+        }
         this.queuedOperations.push({ command, handler, callbacks: { resolve, reject} })
         return
       }
     })
+  }
+
+  private abortTheFlow(command: CommandDefinition['command']) {
+    this.nativeAa2Module.sendCMD(JSON.stringify(command))
+    this.currentOperation = undefined
+    this.unprocessedMessages = []
   }
 
   private onMessage(message: Message) {
